aa93ebd030b53fd1adca375a75d34ca5775addc1
JEEN Lee <jeen@perl.kr>
2010-03-26 13:28:21 +0900

    FilterInstapaper init

---
+++ Filstapaper.pm
@@ -0,0 +1,42 @@
+package Filstapaper;
+use Dancer;
+use URI;
+use URI::Escape;
+
+get '/' => sub {
+    template 'index';
+};
+
+get r( '/filter/(.+)' ) => sub {
+    my ($u) = splat;
+    
+    my $uri = URI->new(uri_unescape($u));
+    my $plugin = load_plugin($uri->host);
+    my $content = $plugin->fetch($uri);
+    $content;
+};
+
+sub load_plugin {
+    my $host = shift;
+
+    (my $pkg = $host) =~ tr/A-Za-z0-9_/_/c;
+
+    open my $fh, "<", "plugins/".$host."/fetch";
+    my $code = join '', <$fh>;
+    my $class = "Filstapaper::Site::$pkg";
+    $code = join "\n", (
+	"package $class;",
+	'use strict;',
+	'use warnings;',
+        "no warnings qw(redefine);",
+        "sub new { bless {}, shift }",
+	"sub site_name { '$host' }",
+	$code,
+	"1;" );
+    eval $code;
+
+    my $plugin = $class->new;
+    return $plugin;
+}
+
+true;
---
+++ public/dispatch.cgi
@@ -0,0 +1,3 @@
+#!/usr/bin/perl
+use Plack::Runner;
+Plack::Runner->run('/Users/jeen/Work/dev/Filstapaper/app.psgi');
