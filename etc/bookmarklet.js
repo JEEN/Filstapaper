bb9e397d9b18b692a5cfe8411660a63634d3e364
JEEN <jeen@perl.kr>
2010-06-08 15:07:20 +0900

    added www.jpnews.kr

--- etc/bookmarklet.js
+++ etc/bookmarklet.js
@@ -1,7 +1,7 @@
 var support_hosts = {
    'blog.daum.net': 1,
    'blog.naver.com': 1,
-   'jpnews.kr': 1,
+   'www.jpnews.kr': 1,
    'www.ddanzi.com': 1,
    'ytn.co.kr': 1,
    'www.devpia.com': 1,
