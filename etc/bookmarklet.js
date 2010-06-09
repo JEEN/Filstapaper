82c0cd6943363c1515af38768a533338de9becb2
JEEN <jeen@perl.kr>
2010-06-09 16:17:07 +0900

    added blog.paran.com

--- etc/bookmarklet.js
+++ etc/bookmarklet.js
@@ -11,6 +11,7 @@ var support_hosts = {
    'www.cbs.co.kr': 1,
    'blog.hani.co.kr': 1,
    'kr.blog.yahoo.com': 1,
+   'blog.paran.com': 1,
 };
 
 function _rlipb846707(){var title,d=document,l=d.location,href=l.href;
