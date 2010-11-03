309ecbec9658e1b45c3f76e698e3dc85e54988be
JEEN <jeen@perl.kr>
2010-11-03 13:25:04 +0900

    supports *.blog.me

--- Filstapaper.pm
+++ Filstapaper.pm
@@ -6,19 +6,20 @@ use Try::Tiny;
 use LWP::Simple ();
 
 get '/' => sub {
-    template 'index', { supports => config->{supports} };
+  template 'index', { supports => config->{supports} };
 };
 
 get qr{ '/barcode/(.+)' } => sub {
-    my ($u) = splat;
-    my $param = params;
-    $u = 'http://'. $u;
-    my $uri = URI->new(uri_unescape($u));
-    delete $param->{splat};
-    if ($param) { 
-	$uri->query_form($param);
-    }
-    template 'barcode', { code => $param->{isbn} };
+  my ($u) = splat;
+  my $param = params;
+  $u = 'http://'. $u;
+  my $uri = URI->new(uri_unescape($u));
+  delete $param->{splat};
+
+  if ($param) { 
+	  $uri->query_form($param);
+  }
+  template 'barcode', { code => $param->{isbn} };
 };
 
 get qr{/imgprx/(.+)} => sub {
@@ -38,50 +39,60 @@ get qr{/imgprx/(.+)} => sub {
 };
 
 get qr{/filter/(.+)} => sub {
-    my ($u) = splat;
-    my $param = params;
+  my ($u) = splat;
+  my $param = params;
 
-    $u = "http://" . $u;    
-    my $uri = URI->new(uri_unescape($u));
-    delete $param->{splat};
-    if ($param) {
-	$uri->query_form($param);
-    }
-    my $content; 
-    try {
-	my $plugin = load_plugin($uri->host);
-        $content = $plugin->fetch($uri);  
-    } catch {
-	redirect $uri, 301;
-   };
-   $content;
+  $u = "http://" . $u;    
+  my $uri = URI->new(uri_unescape($u));
+  delete $param->{splat};
+  if ($param) {
+	  $uri->query_form($param);
+  }
+  my $content; 
+  try {
+	  my $plugin = load_plugin($uri->host);
+    debug $plugin;
+    $content = $plugin->fetch($uri);  
+  } catch {
+    redirect $uri, 301;
+  };
+  $content;
 };
+
+sub get_alias_host {
+  my $host = shift;
+
+  while(my ($match,$alias) = each %{ config->{aliases} }) {
+    return $alias if $host =~ /$match/;
+  }
+}
 
 # Inspired by Plagger::Plugin::Filter::FindEnclosures
 # TODO : Change to preload Plugin Modules / or use Destructor?
 sub load_plugin {
-    my $host = shift;
+  my $host = shift;
 
-    (my $pkg = $host) =~ tr/A-Za-z0-9_/_/c;
+  $host = get_alias_host($host) || $host;
 
-    my $loc = sprintf "plugins/%s/fetch", $host;
-    die unless -f $loc;
-    open my $fh, "<", $loc or die $!;
-    my $code = join '', <$fh>;
-    my $class = "Filstapaper::Site::$pkg";
-    $code = join "\n", (
-	"package $class;",
-	'use strict;',
-	'use warnings;',
-        "no warnings qw(redefine);",
-        "sub new { bless {}, shift }",
-	"sub site_name { '$host' }",
-	$code,
-	"1;" );
-    eval $code;
+  (my $pkg = $host) =~ tr/A-Za-z0-9_/_/c;
+  my $loc = sprintf "plugins/%s/fetch", $host;
+  die unless -f $loc;
+  open my $fh, "<", $loc or die $!;
+  my $code = join '', <$fh>;
+  my $class = "Filstapaper::Site::$pkg";
+  $code = join "\n", (
+    "package $class;",
+	  'use strict;',
+	  'use warnings;',
+    "no warnings qw(redefine);",
+    "sub new { bless {}, shift }",
+	  "sub site_name { '$host' }",
+	  $code,
+	  "1;" );
+  eval $code;
 
-    my $plugin = $class->new;
-    return $plugin;
-}
+  my $plugin = $class->new;
+  $plugin;
+};
 
 true;
